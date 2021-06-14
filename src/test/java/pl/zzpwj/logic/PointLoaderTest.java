0675829d813291b9c7d046598e72949e7826b9bf
Ciachociech <bastanator.rex@gmail.com>
2021-06-15 00:31:29 +0200

    Update PointLoaderTest.java

--- src/test/java/pl/zzpwj/logic/PointLoaderTest.java
+++ src/test/java/pl/zzpwj/logic/PointLoaderTest.java
@@ -15,5 +15,15 @@ public class PointLoaderTest {
     public void PointLoaderReadFile() throws IOException {
         ArrayList<Point> plPoints = pointLoader.readPointsFromJSON();
         Assert.assertEquals(4100, plPoints.size());
+
+        Assert.assertEquals(plPoints.get(0).getId(), Integer.valueOf(752963));
+        Assert.assertEquals(plPoints.get(0).getName(), "Żyrzyn");
+        Assert.assertEquals(22.0917f, plPoints.get(0).getLongitude(), 0.01);
+        Assert.assertEquals(51.49918f, plPoints.get(0).getLatitude(), 0.01);
+
+        Assert.assertEquals(plPoints.get(1).getId(), Integer.valueOf(752967));
+        Assert.assertEquals(plPoints.get(1).getName(), "Żyrardów");
+        Assert.assertEquals(20.44599f, plPoints.get(1).getLongitude(), 0.01);
+        Assert.assertEquals(52.048801f, plPoints.get(1).getLatitude(), 0.01);
     }
 }
