3084e8c231df389aa915c3e3739a2e1102a50a15
Ciachociech <bastanator.rex@gmail.com>
2021-06-17 13:01:54 +0200

    Concept of window

--- src/main/java/pl/zzpwj/app/App.java
+++ src/main/java/pl/zzpwj/app/App.java
@@ -1,4 +1,32 @@
 package pl.zzpwj.app;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class App {
+    private JPanel appPanel;
+    private JButton showHistoryBtn;
+    private JButton findPointBtn;
+    private JButton showForecastBtn;
+    private JPanel mainDataPanel;
+    private JTextField pointTextField;
+    private JComboBox chooseCBox;
+    private JPanel showInformationField;
+    private JTextArea informationTextArea;
+
+    public App() {
+        showHistoryBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("PogodaProject - ZZPWJ");
+        jFrame.setContentPane(new App().appPanel);
+
+    }
 }
